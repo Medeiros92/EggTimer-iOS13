d9d50653b72805576f3af5038c81f92826c8cdab
Medeiros92 <thalesbehringer@gmail.com>
2022-02-21 16:35:13 -0300

    Changing if condition to dictionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -4,21 +4,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes : [String : Int] = ["Soft" : 5, "Medium" : 7, "Hard" : 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle
         
-        if(hardness == "Soft"){
-            Time.text = "\(softTime) Minutes"
-        }else if(hardness == "Medium"){
-            Time.text = "\(mediumTime) Minutes"
-        }else if(hardness == "Hard"){
-            Time.text = "\(hardTime) Minutes"
-        }
+        Time.text =  "\(eggTimes[hardness!]!) Minutes"
     }
     
     @IBOutlet weak var Time: UILabel!
