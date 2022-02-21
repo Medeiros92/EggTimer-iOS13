f8cf05fc4f410fdcc009048ab898bf1be4e1b4a3
Medeiros92 <thalesbehringer@gmail.com>
2022-02-21 13:03:35 -0300

    Add conditional  of the clicking button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -18,7 +18,13 @@ class ViewController: UIViewController {
         
         let hardness = sender.currentTitle
         
-        
+        if(hardness == "Soft"){
+            Time.text = "\(softTime) Minutes"
+        }else if(hardness == "Medium"){
+            Time.text = "\(mediumTime) Minutes"
+        }else if(hardness == "Hard"){
+            Time.text = "\(hardTime) Minutes"
+        }
     }
     
     @IBOutlet weak var Time: UILabel!
