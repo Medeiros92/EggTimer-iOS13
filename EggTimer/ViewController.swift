8dbfe77777d69cc7f6c0d417db7cbe50b826ff4a
Medeiros92 <thalesbehringer@gmail.com>
2022-02-21 22:40:04 -0300

    Update Count Time, change title, play sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,12 +1,15 @@
 
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes : [String : Int] = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    let eggTimes : [String : Int] = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
     
-    var secondRemaining = 60
+    var totalTime = 0
+    var secondsPassed = 0
+    var player : AVAudioPlayer!
     
     var timer = Timer()
     
@@ -16,23 +19,38 @@ class ViewController: UIViewController {
         
         let hardness = sender.currentTitle!
         
-       secondRemaining =  eggTimes[hardness]!
+       totalTime =  eggTimes[hardness]!
         
-        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
 
     }
     
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
     @objc func updateTimer() {
-        if secondRemaining >= 0 {
-            Time.text = "\(secondRemaining) seconds"
-            secondRemaining -= 1
-            if secondRemaining == 0{
-                titleText.text = "Your egg is ready!"
-            }
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Done!"
+            playSound()
         }
     }
     
-    @IBOutlet weak var Time: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var titleText: UILabel!
 }
